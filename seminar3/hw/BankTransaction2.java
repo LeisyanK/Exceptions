44bd1bd9b56084e94c78f2896fb253425121d89b
Kashapova Leisyan <LeisyanRA@mail.ru>
2023-07-05 17:32:59 +0500

    seminar3

--- seminar3/hw/BankTransaction2.java
+++ seminar3/hw/BankTransaction2.java
@@ -3,31 +3,39 @@ package seminar3.hw;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class BankTransaction {
+public class BankTransaction2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         double balance = 1000.50;
+        Scanner sc = new Scanner(System.in);
         try {
             System.out.println("Введите сумму перевода: ");
             double amount = sc.nextDouble();
-            if (amount <= 0) {
-                throw new InvalidAmountException("Некорректная сумма перевода");
-            }
-            if (amount > balance) {
-                throw new InsufficientFundsException("Недостаточно средств на счете");
-            }
+            checkAmount(amount, balance);
             System.out.println("Перевод выполнен успешно.");
-        } catch (InvalidAmountException e) {
-            System.out.println("Ошибка: " + e.getMessage());
         } catch (InputMismatchException e) {
-            // throw new RuntimeException(e); // автоподстановка IDE: предлагает перевести checked в unchecked
+            // throw new RuntimeException(e);
             System.out.println("Ошибка: некорректный ввод");
-        } catch (InsufficientFundsException e) {
-            System.out.println("Ошибка: " + e.getMessage());
+        } catch (InvalidAmountException e) {
+            // TODO Auto-generated catch block
             // e.printStackTrace();
+            System.out.println("Ошибка: " + e.getMessage());
+        } catch (InsufficientFundsException e) {
+            // TODO Auto-generated catch block
+            // e.printStackTrace();
+            System.out.println("Ошибка: " + e.getMessage());
         } finally {
             sc.close();
         }
+    }
+
+    public static void checkAmount (double amount, double balance) 
+    throws InvalidAmountException, InsufficientFundsException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Некорректная сумма перевода");
+        }
+        if (amount > balance) {
+                throw new InsufficientFundsException("Недостаточно средств на счете");
+        }            
     }
 }
 
